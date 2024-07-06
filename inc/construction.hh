262e5a3cfbb940f3fcf25ec32dffa35982ab38dc
kirtikeshkumar <kumarkirtikesh@gmail.com>
2024-07-06 20:04:47 +0530

    Added new branch to run simulations for sheilding by single sheet of material currently modified the detector geometry need to modify the data saving

--- inc/construction.hh
+++ inc/construction.hh
@@ -1,6 +1,6 @@
 /*This is the construction header file where we define the class, variables and functions required for the construction of the environment and the detector*/
 
-//First we include all the necessary header files
+// First we include all the necessary header files
 #ifndef CONSTRUCTION_HH
 #define CONSTRUCTION_HH
 
@@ -19,56 +19,48 @@
 #include "G4SubtractionSolid.hh"
 #include "G4UnionSolid.hh"
 
-//Here we define the DetectorConstruction class and its variables and functions
+// Here we define the DetectorConstruction class and its variables and functions
 class MyDetectorConstruction : public G4VUserDetectorConstruction
 {
 public:
-	MyDetectorConstruction();//The constructor and destructor function of the class
-	MyDetectorConstruction(std::string str);
+	MyDetectorConstruction(); // The constructor and destructor function of the class
+	MyDetectorConstruction(char mat, std::string th);
 	~MyDetectorConstruction();
-	
-	G4LogicalVolume *GetScoringVolume() const {return fScoringVolume;}
-	
-	//This is the Construct function of the type G4VPhysicalVolume which is used to construct the detector and environment with the required dimenstions and properties
+
+	G4LogicalVolume *GetScoringVolume() const { return fScoringVolume; }
+
+	// This is the Construct function of the type G4VPhysicalVolume which is used to construct the detector and environment with the required dimenstions and properties
 	virtual G4VPhysicalVolume *Construct();
+
 private:
-//Here we define three variables for the Size of the Environment, the size of the detector and the position of the detector inside the environment
-	
-	G4Box *solidWorld;
-	G4Box *detVol;
-	G4LogicalVolume *logicWorld, *logicshell, *logicdetVol;
-	G4VPhysicalVolume *physWorld, *physshell, *physdetVol;
-	G4VPhysicalVolume *physshellMuVetoOut, *physshellHDPEOuter, *physshellBP, *physshellLead, *physshellHDPEInner, *physshellMuVetoIn, *physshellCuColdBox;
-	G4LogicalVolume *logicshellMuVetoOut, *logicshellHDPEOuter, *logicshellBP, *logicshellLead, *logicshellHDPEInner, *logicshellMuVetoIn, *logicshellCuColdBox;
-	G4VPhysicalVolume *physshell0,*physshell1,*physshell2,*physshell3,*physshell4,*physshell5,*physshell6,*physshell7;
-	G4LogicalVolume *logicshell0, *logicshell1, *logicshell2, *logicshell3, *logicshell4, *logicshell5, *logicshell6, *logicshell7;
-	G4VPhysicalVolume *physSteelPlate, *physUnderSideBP, *physUnderSideHDPE;
-	G4LogicalVolume *logicSteelPlate, *logicUnderSideBP, *logicUnderSideHDPE;
-	
+	// Here we define three variables for the Size of the Environment, the size of the detector and the position of the detector inside the environment
+
+	G4Box *solidWorld, *solidSheet;
+	G4VSolid *detVol;
+	G4LogicalVolume *logicWorld, *logicSheet, *logicdetVol;
+	G4VPhysicalVolume *physWorld, *physSheet, *physdetVol;
+
 	G4Material *worldMat, *myTolueneMat, *BoratedPE, *HDPE, *Lead, *Copper, *Vaccum, *Steel;
-	
-	std::map<char, G4Material*> MatMap;	
-	std::vector<G4Material*> LogicArrangement;
+	G4Material *sheildMat;
+
+	std::map<char, G4Material *>
+		MatMap;
+	std::vector<G4Material *> LogicArrangement;
 
 	void DefineMaterials();
-	
-	G4VSolid* ConstructShell(double xsz, double ysz, double zsz, double thickness, double offset);
-	G4VSolid* SubtractBox(G4VSolid* sol, G4ThreeVector Boxsz, G4ThreeVector relPosSubVol);
-	G4VSolid* SubtractBoxfrmShell(G4ThreeVector shellsz, double thickness, double offset, G4ThreeVector Boxsz, G4ThreeVector relPosSubVol);
-	G4VSolid* SubtractBoxfrmShell(G4ThreeVector shellsz, double thickness, double offset, G4ThreeVector Boxsz);
-	
+
 	virtual void ConstructSDandField();
-	
+
 	G4GenericMessenger *fMessenger;
-	
+
 	G4LogicalVolume *fScoringVolume;
-	
-	void ConstructSetup();
-	void ConstructSetupMod();
-	void ConstructSetupV1();
-	
-	G4double xWorld,yWorld,zWorld,xloc;
-	
+
+	void ConstructSingleSheet();
+
+	G4VSolid *ConstructShell(double xsz, double ysz, double zsz, double thickness, double offset);
+
+	G4double xWorld, yWorld, zWorld, xloc, width;
+
 	G4OpticalSurface *mirrorSurface;
 };
 
--- mac/run.sh
+++ mac/run.sh
@@ -8,26 +8,27 @@ range(){
 
 NUMOFTHREADS=15
 ENERGIES=$(range 100 10000 100)
-PARTICLES="gamma" #("neutron" "gamma")
+PARTICLES=("neutron" "gamma" "e+" "e-")
 NUMOFEVENTS=10000000
-ORDERING=("HHBBLLHH" "HHBLHBLH")
-ORDER="HHBLHBLH"
+MATERIALS=("H" "L" "B")\
+THICKNESSES=(1 2 5 10 15 20)
+#ORDERING=("HHBBLLHH" "HHBLHBLH")
+#ORDER="HHBLHBLH"
 #ORDER="HHBBLLHH"
 
-for PARTICLE in $PARTICLES;do
-    for ENERGY in $ENERGIES;do
-        sed -e "s|NUMOFTHREADS|$NUMOFTHREADS|g" \
-            -e "s|ENERGY|$ENERGY|g" \
-            -e "s|PARTICLE|$PARTICLE|g" \
-            -e "s|NUMOFEVENTS|$NUMOFEVENTS|g" \
-            run.template.mac > run.mac
-    	if [$ORDER]
-	then
-		./sim run.mac $ORDER
-	else
-		./sim run.mac
-	fi
-    	hadd output_${PARTICLE}_${ENERGY}keV_${ORDER}_${NUMOFEVENTS}evt.root output0_t*.root
-    	rm output0_t*
+for MATERIAL in MATERIALS;do
+    for THICKNESS in THICKNESSES;do
+        for PARTICLE in $PARTICLES;do
+            for ENERGY in $ENERGIES;do
+                sed -e "s|NUMOFTHREADS|$NUMOFTHREADS|g" \
+                    -e "s|ENERGY|$ENERGY|g" \
+                    -e "s|PARTICLE|$PARTICLE|g" \
+                    -e "s|NUMOFEVENTS|$NUMOFEVENTS|g" \
+                    run.template.mac > run.mac
+		        ./sim run.mac $MATERIAL $THICKNESS
+    	        hadd output_${PARTICLE}_${ENERGY}keV_${ORDER}_${NUMOFEVENTS}evt.root output0_t*.root
+    	        rm output0_t*
+            done
+        done
     done
 done
